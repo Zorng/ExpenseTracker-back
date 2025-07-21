612b7ecfee40d4cd71d0b4d17ed7df83674e0f0f
Tong Vorak <vorak.tong@student.cadt.edu.kh>
2025-07-21 21:56:18 +0700

    Complete for user and add authentication

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import db from './models/index.js';
 import {serveSwagger, setupSwagger} from "./config/swagger.js";
+import categoryRoutes from './routes/Category.route.js';
+import userRoutes from './routes/User.route.js';
 
 dotenv.config();
 
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/docs', serveSwagger, setupSwagger);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Expense Tracker API');
