222bd33de35c570f771288d5cc64815d82ef2296
Tong Vorak <vorak.tong@student.cadt.edu.kh>
2025-07-26 22:56:54 +0700

    update

--- utils/emailService.js
+++ utils/emailService.js
@@ -5,7 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
+        user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD
     }
 });
